71c4978cae6f53a51155351f34ed26317f0ed627
cholobi_ <nassorocholobi@gmail.com>
2024-08-16 14:12:40 +0300

    fix deployment

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -7,7 +7,7 @@ const AddTransaction = () => {
     <div className='w-full lg:w-80 md:w-96 bg-green-500 flex flex-col gap-y-2 px-4 py-1'>
       <h1>Add transaction</h1>
       <hr className='border-gray-100 ' />
-      <form action={clientAction} className="bg-yellow-500 ">
+      <form action={clientAction} className='bg-yellow-500 '>
         <h1>form</h1>
       </form>
     </div>
